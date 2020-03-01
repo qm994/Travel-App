06c4316b8e10e13c8fece332c528f6bed7e1c7b7
Qingyuan Ma <qm28@georgetown.edu>
2020-02-29 21:21:51 -0500

    add the test

---
+++ src/client/js/updateUI.test.js
@@ -0,0 +1,6 @@
+const updateUI = require("./updateUI");
+const regeneratorRuntime = require("regenerator-runtime");
+
+test("it should be true", async() => {
+    expect(updateUI).toBeDefined();
+})
